97e0c5a363ec441db318bed0ba359c32ab5c740e
EroCrizs <aemielvin.loremia@gmail.com>
2015-10-30 11:42:20 +0800

    Add a sliding animation to showing and hiding of student-only detail in the sign-up

--- res/js/front-end.js
+++ res/js/front-end.js
@@ -5,15 +5,23 @@ $(document).ready(function(){
     $("input:radio[name='account-type']").change(function(){  
 
             if(this.value == '2' && this.checked){
-              $("#studentOnly").show();
+              $("#studentOnly").show("slow", null);
 			  $("#yearncourse").attr("required", true);
 			  $("#nickname").attr("required", true);
 			  $("#birthday").attr("required", true);
+			  
+			  $("#yearncourse").attr("disabled", false);
+			  $("#nickname").attr("disabled", false);
+			  $("#birthday").attr("disabled", false);
             }else{
-              $("#studentOnly").hide();
+              $("#studentOnly").hide("slow", null);
 			  $("#yearncourse").attr("required", false);
 			  $("#nickname").attr("required", false);
 			  $("#birthday").attr("required", false);
+			  
+			  $("#yearncourse").attr("disabled", true);
+			  $("#nickname").attr("disabled", true);
+			  $("#birthday").attr("disabled", true);
             }
 
     });
